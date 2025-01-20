3f466ae4e92b47c5bd803ce773f1a6a007456cc0
Linda Collins <lindanwajei2205@gmail.com>
2025-01-20 22:37:45 +0000

    Refactor blog views and update URL patterns for improved readability

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from blog.views import my_blog
 
 # Create your views here.
 def my_blog(request):
     return HttpResponse("Hello, Blog!")
+
+
+
--- codestar/urls.py
+++ codestar/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 from blog.views import my_blog
 
+
+
+
+
+
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('blog/', my_blog, name='blog'),
+    
 ]
