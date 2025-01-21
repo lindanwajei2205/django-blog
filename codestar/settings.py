b09c0b16683a1de36a610112a8880bfab1a28203
Linda Collins <lindanwajei2205@gmail.com>
2025-01-21 11:50:18 +0000

    Update allowed_hosts in settings for production environment

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&kjsc3))+nb4v3_vcusj!6jq949#vzrd181)ybz3$s+w$_*991
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '8000','.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition
