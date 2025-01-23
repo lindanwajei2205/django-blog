9e694317d60192fc34ee59e1375461c62f0984a8
Linda Collins <lindanwajei2205@gmail.com>
2025-01-23 14:17:33 +0000

    Update Post model to add created_on and updated_on fields; modify excerpt field to allow null values

--- blog/models.py
+++ blog/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField(default='')
-    created_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
+    excerpt = models.TextField(blank=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True)    
