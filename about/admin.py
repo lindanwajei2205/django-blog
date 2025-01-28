dee9b5cdd2b14a70bb2ac9dedb14361281fb2cd5
Linda Collins <lindanwajei2205@gmail.com>
2025-01-28 22:23:23 +0000

    Add comment CRUD

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -8,3 +8,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class AboutAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    list_display = ('message', 'read',)
